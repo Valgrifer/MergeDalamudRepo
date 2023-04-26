2fadb084af2770d0dd924c0865dabd3558c3346c
Valgrifer <valgrifer123@gmail.com>
2023-04-26 17:38:44 +0200

    InitRepository

---
+++ config.js
@@ -0,0 +1,44 @@
+const config = {
+    DalamudApiLevel: 8,
+    BlackListPlugin: [
+        "Splatoon",
+        "DSREyeLocator",
+        "AbyssosToolbox",
+        "Athavar.FFXIV.Plugin",
+        "BossMod",
+    ],
+    freezeFilePath: "repo.json"
+};
+
+
+// noinspection JSValidateTypes
+/**  @type RepoItem */
+const defaultInfoPlugin = {
+    Author: "Valgrifer",
+        Name: "DalamudRepoInfo",
+        InternalName: "DalamudRepoInfo",
+        AssemblyVersion: "0.0.0.1",
+        Description: "",
+        ApplicableVersion: "any",
+        RepoUrl: "https://steam.valgrifer.fr/DalamudRepo/",
+        DalamudApiLevel: config.DalamudApiLevel,
+        LoadPriority: 0,
+        Punchline: "Information sur ce script",
+        ImageUrls: null,
+        IconUrl: "",
+        IsHide: "False",
+        IsTestingExclusive: "False",
+        DownloadCount: 0,
+        CategoryTags: [],
+        LastUpdate: Date.now() / 1000,
+        DownloadLinkInstall: "https://steam.valgrifer.fr/DalamudRepo/404.zip",
+        DownloadLinkUpdate: "https://steam.valgrifer.fr/DalamudRepo/404.zip",
+        Tags: [
+        "repo",
+        "info",
+    ]
+}
+export default {
+    ...config,
+    defaultInfoPlugin
+};
---
+++ main.js
@@ -0,0 +1,101 @@
+// noinspection JSCheckFunctionSignatures
+
+import Repolist from "./repolist.js";
+import Config from "./config.js";
+import fetch from "node-fetch";
+import fs from "fs";
+
+const SETTINGS = { method: "Get" };
+
+/**
+ * @typedef {Object} RepoItem
+ * @property {undefined|string} Author
+ * @property {undefined|string} Name
+ * @property {undefined|boolean} Disabled
+ * @property {undefined|string} Description
+ * @property {undefined|string} InternalName
+ * @property {undefined|string} AssemblyVersion
+ * @property {undefined|string} TestingAssemblyVersion
+ * @property {undefined|string} RepoUrl
+ * @property {undefined|string} ApplicableVersion
+ * @property {undefined|number} DalamudApiLevel
+ * @property {undefined|string} DownloadLinkInstall
+ * @property {undefined|string} DownloadLinkTesting
+ * @property {undefined|string} DownloadLinkUpdate
+ * @property {undefined|string} IconUrl
+ * @property {undefined|string[]} Tags
+ *
+ * @property {string} originRepository
+ * @property {undefined|string} error
+ * @property {undefined|number} code
+ * @property {undefined|any} err
+ */
+
+(async () => {
+    /** @type {RepoItem[]} */
+    let errorList = [];
+    /** @type {RepoItem[]} */
+    let finalList = [];
+
+    /** @type {RepoItem[]} */
+    const RepoListResult = (await Promise.all(Repolist.map(originRepository => fetch(originRepository, SETTINGS)
+        .then(async res => {
+            if (res.status === 200)
+                return (await res.json())
+                    .map(item => ({
+                        ...item,
+                        originRepository,
+                        Description: (item.Description ? item.Description + '\n\n' : '') + "Plugin from " + originRepository
+                    }));
+            return [
+                {
+                    repo: originRepository,
+                    code: 404,
+                    error: "Not Found",
+                }];
+        })
+        .catch(err => ([
+            {
+                repo: originRepository,
+                code: 500,
+                error: "Une erreur c'est produit",
+                err
+            }])))))
+        .reduce((el1, el2) => el1 ? ([...el1, ...el2]) : el2);
+
+    RepoListResult.filter(item =>
+            !item.Disabled && !Config.BlackListPlugin.includes(item.InternalName) && item.DalamudApiLevel && item.DalamudApiLevel >= Config.DalamudApiLevel
+        ).forEach(item => {
+            if(item.error)
+            {
+                errorList.push(item);
+                return;
+            }
+            let same;
+            if((same = finalList.find(el => el.InternalName === item.InternalName)))
+            {
+                return;
+            }
+            finalList.push(item);
+        });
+
+    let info = Config.defaultInfoPlugin;
+    /** @type {function(RepoItem)} */
+    const printer = item => {
+        info.Description += "\n- " + item.originRepository;
+    };
+
+    info.Description = "404: ";
+    errorList.filter((item) => item.code === 404)
+        .forEach(printer);
+
+    info.Description += "\n\n\n500: ";
+    errorList.filter((item) => item.code === 500)
+        .forEach(printer);
+
+    console.error(info.Description);
+
+    finalList.push(info);
+
+    fs.writeFileSync(Config.freezeFilePath, JSON.stringify(finalList))
+})()
