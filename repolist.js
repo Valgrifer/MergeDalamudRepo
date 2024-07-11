df6fe439fbee07b40b41a90d303a315ed3a32486
Valgrifer <valgrifer123@gmail.com>
2024-07-11 11:04:50 +0200

    Update repolist.js

--- repolist.js
+++ repolist.js
@@ -94,6 +94,7 @@ export default [
     "https://raw.githubusercontent.com/KangasZ/DalamudPluginRepository/main/plugin_repository.json",
     "https://puni.sh/api/repository/herc",
     "https://puni.sh/api/repository/veyn",
+    "https://raw.githubusercontent.com/FFXIV-CombatReborn/CombatRebornRepo/main/pluginmaster.json",
     "https://raw.githubusercontent.com/ArchiDog1998/DalamudPlugins/main/pluginmaster.json",
     "https://github.com/plottingCreeper/MyDalamudPlugins/raw/master/pluginmaster.json",
     "https://puni.sh/api/repository/jukka",
